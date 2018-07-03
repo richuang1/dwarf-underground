bb5bbc2d3874c8d7361a73fb014af4cd9a320424
richuang1 <richuang@iu.edu>
2018-07-03 10:38:30 -0400

    6 components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Header from './Header'
+import Header from './Header' ;
+import Article from './Article' ;
+import Bottom from './Bottom';
+import ArticleLink from './ArticleLink';
+import AdImage from './AdImage';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
@@ -19,7 +24,7 @@ class App extends Component {
                 <p className="date">on 28 April, 3018 of the Third Age</p>
               </div>
             </div>
-            <section className="article-body">
+            <section>
               <p><span className="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
 
               <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
